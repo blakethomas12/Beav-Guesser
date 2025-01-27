3a46bd74bca3812255aba64a5e78fd4648422c5e
Blake Thomas <blakeathomas12@yahoo.com>
2025-01-27 15:13:45 -0800

    added function to return number of locations and pull location by number

--- db.js
+++ db.js
@@ -11,8 +11,7 @@ db.once('open', function(){
 const locationSchema = new mongoose.Schema({
     path: String,
     long: Number,
-    lat: Number,
-    UserSubmitted: Boolean
+    lat: Number
 })
 
 const Location = mongoose.model('location', locationSchema)
@@ -20,7 +19,30 @@ const Location = mongoose.model('location', locationSchema)
 
 const userSchema = new mongoose.Schema({
     username: String,
+    password: String,
     level: Number,
 })
 
 const User = mongoose.model('users', userSchema)
+
+
+//calling this functin returns the total number of locations
+async function get_num_locations() {
+    const count = await Location.countDocuments()
+    return count
+}
+
+//provide number for which document to grab
+async function get_location_by_number(num) {
+    try{
+        const num_locations = await get_num_locations()
+
+        if(num < 0 || num >=num_locations) return null
+
+        const doc = await Location.findOne().skip(num).exec()
+        return doc
+    } catch(error){
+            console.error('error retrieving document: (check bounds)', error)
+            throw error
+    }
+}
