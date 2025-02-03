fe594a611d9cedf2fad5b152de0922fdc03ce360
Lukas Martinek <lukas.martinek@gmail.com>
2025-02-03 13:15:33 -0800

    Added guess canvas, latlngtoxy function, and draw guess and check guess function. Not working completely right now.

--- Front End Files/Map_Testing.js
+++ Front End Files/Map_Testing.js
@@ -1,34 +1,59 @@
+let actualLat, actualLng;
+let guessX, guessY;
+
+const mapBounds = {
+  topLeft: { lat: 44.56766730220258, lng: -123.28959983789187 },
+  bottomRight: { lat: 44.55977402745042, lng: -123.2750217935866 },
+};
+
 function getRandomStreetViewEmbedLink() {
-    // define bounding box coordinates
-    const bounds = {
-      topLeft: { lat: 44.56766730220258, lng: -123.28959983789187 },
-      topRight: { lat: 44.56725015537913, lng: -123.2722258248906 },
-      bottomRight: { lat: 44.55977402745042, lng: -123.2750217935866 },
-      bottomLeft: { lat: 44.55767469824912, lng: -123.2896888691443 },
-    };
-  
     // generate random latitude and longitude within bounds
-    const randomLat =
-      Math.random() * (bounds.topLeft.lat - bounds.bottomLeft.lat) +
-      bounds.bottomLeft.lat;
-    const randomLng =
-      Math.random() * (bounds.topRight.lng - bounds.topLeft.lng) +
-      bounds.topLeft.lng;
-  
-    // construct street view embed link
-    const embedLink = `https://www.google.com/maps/embed?pb=!4v0!6m8!1m7!1sPLACEHOLDER!2m2!1d${randomLat.toFixed(
-      6
-    )}!2d${randomLng.toFixed(
-      6
-    )}!3f0!4f0!5f0.7820865974627469`;
-  
-    return embedLink;
+    actualLat = Math.random() * (mapBounds.topLeft.lat - mapBounds.bottomRight.lat) + mapBounds.bottomRight.lat;
+    actualLng = Math.random() * (mapBounds.bottomRight.lng - mapBounds.topLeft.lng) + mapBounds.topLeft.lng;
+
+    return `https://www.google.com/maps/embed?pb=!4v0!6m8!1m7!1sPLACEHOLDER!2m2!1d${actualLat.toFixed(6)}!2d${actualLng.toFixed(6)}!3f0!4f0!5f0.7820865974627469`;
   }
 
   function loadRandomStreetView() {
-    const iframe = document.getElementById("streetViewFrame");
-    const randomLink = getRandomStreetViewEmbedLink();
-    iframe.src = randomLink;
+    const iframe = document.getElementById("street-view-frame");
+    iframe.src = getRandomStreetViewEmbedLink();
   }
 
   window.onload = loadRandomStreetView;
+
+  function latLngToXY(lat, lng, imgWidth, imgHeight) {
+    const x = ((lng - mapBounds.topLeft.lng) / (mapBounds.bottomRight.lng - mapBounds.topLeft.lng)) * imgWidth;
+    const y = ((mapBounds.topLeft.lat - lat) / (mapBounds.topLeft.lat - mapBounds.bottomRight.lat)) * imgHeight;
+    return { x, y };
+}
+
+document.getElementById("overlay-map").addEventListener("click", function(event) {
+  const rect = event.target.getBoundingClientRect();
+  guessX = event.clientX - rect.left;
+  guessY = event.clientY - rect.top;
+  drawGuess(guessX, guessY, "blue");
+});
+
+function drawGuess(x, y, color) {
+  const canvas = document.getElementById("guess-canvas");
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = canvas.clientWidth;
+  canvas.height = canvas.clientHeight;
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.arc(x, y, 10, 0, 2 * Math.PI);
+  ctx.fill();
+}
+
+function checkGuess() {
+  const img = document.getElementById("overlay-map");
+  const { x: actualX, y: actualY } = latLngToXY(actualLat, actualLng, img.clientWidth, img.clientHeight);
+
+  drawGuess(actualX, actualY, "red");
+
+  const distance = Math.sqrt((actualX - guessX) ** 2 + (actualY - guessY) ** 2);
+  document.getElementById("feedback").innerText = `Distance: ${Math.round(distance)} pixels`;
+}
