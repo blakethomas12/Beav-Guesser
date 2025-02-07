526c4a78eb90d0da0a2b2f755d4b5f3d9484bc15
kevintran02 <happy65435@gmail.com>
2025-02-07 10:28:24 -0800

    Added leaderboard function

--- Backend Files/server.js
+++ Backend Files/server.js
@@ -168,6 +168,16 @@ app.post("/getLocation", async function (req, res) {
   }
 });
 
+app.get("/leaderboard", async (req, res) => {
+  try {
+    const leaderboard = await dbFunctions.calculate_total_scores();
+    res.status(200).render("leaderboard", { leaderboard });
+  } catch (error) {
+    console.error("Error rendering leaderboard:", error);
+    res.status(500).send("Error loading leaderboard");
+  }
+});
+
 app.listen(port, function (err) {
   if (err) {
     throw err;
