8a2b26153be83e4fc00bddaefd815ad45cb8607e
Blake Thomas <blakeathomas12@yahoo.com>
2025-02-19 15:19:56 -0800

    testing lint config file

--- Backend Files/eslint.config.js
+++ Backend Files/eslint.config.js
@@ -1,15 +1,16 @@
-module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-    },
-    extends: 'eslint:recommended',
-    parserOptions: {
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    rules: {
-     
-    },
-  };
-  
+import { defineConfig } from 'eslint-define-config';
+
+export default defineConfig({
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add your custom rules here
+  },
+});
