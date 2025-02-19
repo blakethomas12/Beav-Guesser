25f85de7bbc4250d0d8f77347a800cd9d5bed118
Blake Thomas <blakeathomas12@yahoo.com>
2025-02-19 15:16:37 -0800

    testing new config file

--- Backend Files/eslint.config.js
+++ Backend Files/eslint.config.js
@@ -1,21 +1,15 @@
-// eslint.config.js
-import { defineConfig } from 'eslint-define-config';
-
-export default defineConfig({
-  parserOptions: {
-    ecmaVersion: 2021,
-  },
-  env: {
-    es2021: true,
-    node: true,
-    browser: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  rules: {
-    // Your custom rules
-  },
-});
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    rules: {
+     
+    },
+  };
+  
