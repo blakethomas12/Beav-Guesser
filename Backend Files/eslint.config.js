e395895fdd0dc4c6e29c93e49ab4ab6e7f8c183a
Blake Thomas <blakeathomas12@yahoo.com>
2025-02-19 15:36:23 -0800

    got eslint to work just need to add rules

--- Backend Files/eslint.config.js
+++ Backend Files/eslint.config.js
@@ -1,16 +1,8 @@
+// eslint.config.js
 import { defineConfig } from 'eslint-define-config';
 
 export default defineConfig({
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
   rules: {
-    // Add your custom rules here
+    // Your custom rules
   },
 });
